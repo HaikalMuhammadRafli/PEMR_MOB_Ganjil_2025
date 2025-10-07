eb5fd4c7b3c7b07922d38189f455ad40219b0d78
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2025-10-07 14:56:54 +0700

    feat: P01 langkah 4

--- Week 06/layout_nav_flutter/lib/main.dart
+++ Week 06/layout_nav_flutter/lib/main.dart
@@ -7,15 +7,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text('Batu, Malang, Indonesia', style: TextStyle(color: Colors.grey[500])),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          Icon(Icons.star, color: Colors.red[500]),
+          const Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Haikal Muhammad Rafli - 2341720008',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        appBar: AppBar(title: const Text('Flutter layout demo')),
+        body: Column(children: [titleSection]),
       ),
     );
   }
